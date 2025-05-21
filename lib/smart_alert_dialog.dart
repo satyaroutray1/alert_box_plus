d62697e8b8b565de507f5d27ca675e32f4204499
satya <sroutraykec@gmail.com>
2025-05-21 10:25:25 +0530

    Initial commit

---
+++ lib/smart_alert_dialog.dart
@@ -0,0 +1,3 @@
+library smart_alert_dialog;
+
+export 'src/smart_alert_dialog.dart';
---
+++ test/smart_alert_dialog_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:smart_alert_dialog/smart_alert_dialog.dart';
+
+void main() {
+
+}
