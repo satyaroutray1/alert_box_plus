08d55f1d1d5793fb395034e1e23fec76b84af860
satya <sroutraykec@gmail.com>
2025-05-21 10:28:36 +0530

    basic

---
+++ lib/src/smart_alert_dialog.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SmartAlertDialog extends StatefulWidget {
+  const SmartAlertDialog({super.key});
+
+  @override
+  State<SmartAlertDialog> createState() => _SmartAlertDialogState();
+}
+
+class _SmartAlertDialogState extends State<SmartAlertDialog> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
